b59be8af5f91b71fae40decf27969a9fb880c6d0
Apujado <pujadoaurelie@orange.fr>
2025-06-04 17:46:43 +0200

    ajustements avant remplissage

--- pages/2_📊_Analyses des données.py
+++ pages/2_📊_Analyses des données.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Analyse data", page_icon="📊",layout="wide")
+st.set_page_config(page_title="Data", page_icon="📊",layout="wide")
 
 st.title("Data 4 U")
 st.subheader("Analyse de la base de données")
@@ -11,6 +11,6 @@ L'objectif est d'identifier des éléments clés qui peuvent influencer la progr
 
 import pandas as pd 
 df = pd.read_parquet('C:/Users/pujad/OneDrive - APS Consult/Documents/FORMATION/Wild Code School/CREUSE_CINE_PLUS/films_groupes.parquet')
-st.dataframe(df.head(10)) 
+
 
 
