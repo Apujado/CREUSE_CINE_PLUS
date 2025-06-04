b4264605b2e8574cfc1cef8d1a7b4ff4fd71be38
Apujado <pujadoaurelie@orange.fr>
2025-06-04 17:02:57 +0200

    organisation

--- pages/Data.py
+++ pages/Data.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import pandas as pd 
-df = pd.read_parquet('C:/Users/pujad/OneDrive - APS Consult/Documents/FORMATION/Wild Code School/CREUSE_CINE_PLUS/films_groupes.parquet')
+
 st.set_page_config(page_title="Analyse data", page_icon="📊",layout="wide")
 
 st.title("Data 4 U")
@@ -9,6 +8,9 @@ st.write("""
 Cette section présente une analyse approfondie de la base de données des films, mettant en évidence les tendances et caractéristiques spécifiques. 
 L'objectif est d'identifier des éléments clés qui peuvent influencer la programmation du cinéma.
 """)
+
+import pandas as pd 
+df = pd.read_parquet('C:/Users/pujad/OneDrive - APS Consult/Documents/FORMATION/Wild Code School/CREUSE_CINE_PLUS/films_groupes.parquet')
 st.dataframe(df.head(10)) 
 
 
